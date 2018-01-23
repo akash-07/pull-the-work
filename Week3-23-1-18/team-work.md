e25e2e6b5df58a825e1d9d80fd00acfa65c7b8b5
akash-07 <akashdhasade@gmail.com>
2018-01-23 06:05:53 +0530

    Wrote WSR

---
+++ Week3-23-1-18/team-work.md
@@ -0,0 +1,21 @@
+# Week3 16-1-18 to 23-1-18
+
+**Tool:** Prioritizing pull requests via machine learning techniques
+
+**Members:** Akash, Bhargav and Sumanth
+
+**Team Number:** 1
+
+| Goals     | Type     | Status| Hours Spent |
+| :------------- | :------------- | :---------------| :--------|
+| Thorough reading of research paper| Preparation| Done| 3 |
+| Understanding how pull requests work| Preparation| Done| 1 |
+| Laying down metrics for prioritizing PR | Requirements| Done| 1-2 |
+| Collecting PR data from github | Requirements| Done| 4-5 |
+| Text classification via ML on titles of PR| Implementation| Next week| -|
+
+**Next week Goals:**
+
+1. Classify pull requests into buckets like bug fixing, new feature addition, etc.
+2. This is achieved by text classification ML techniques applied on titles of pull requests.
+3. Prioritize pull requests based on above classification (like assigning higher priority to bug fixing over feature addition)
--- python-scripts/new-scripts/PR_categorize.py
+++ python-scripts/new-scripts/PR_categorize.py
@@ -4,19 +4,22 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
-os.chdir("E:\\6th sem pse lab\pull-the-work\python-scripts\\new-scripts")
+#os.chdir("E:\\6th sem pse lab\pull-the-work\python-scripts\\new-scripts")
 tag = pd.read_csv("tensorflow.csv")
-X=tag.title
-y=tag.priority
-X_train,X_test,y_train,y_test = train_test_split(X,y, random_state=1)
+#gets you column named 'title', same as tag['title']
+X = tag.title
+y = tag.priority
+#splitting data 75% (train), 25% test
+X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=1)
+# Machine learning model, parameters?
 vect = CountVectorizer(ngram_range(1,3))
+# Fitting train data
 vect.fit(X_train)
 X_train_dtm = vect.transform(X_train)
 X_test_dtm = vect.transform(X_test)
 nb = MultinomialNB()
 nb.fit(X_train_dtm, y_train)
-y_pred_class=nb.predict(X-test_dtm)
+y_pred_class = nb.predict(X-test_dtm)
 print(metrics.accuracy_score(y_test, y_pred_class))
 y_pred_prob=nb.predict_proba(X_test_dtm)
 print(y_pred_prob)
-
