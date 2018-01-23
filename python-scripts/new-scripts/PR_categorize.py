efa82fe93e587568222f6d4dad74434a185f59bb
K.S.S.Bhargav- TCS15B015 <tcs15b015@iittp.ac.in>
2018-01-23 08:00:03 +0530

    "Small Fixes"

--- python-scripts/new-scripts/PR_categorize.py
+++ python-scripts/new-scripts/PR_categorize.py
@@ -5,11 +5,11 @@ from sklearn.cross_validation import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 os.chdir("E:\\6th sem pse lab\pull-the-work\python-scripts\\new-scripts")
-tag = pd.read_csv("tensorflow.csv")
+tag = pd.read_csv("tensorflow.csv", encoding= "latin-1")
 X=tag.title
 y=tag.priority
 X_train,X_test,y_train,y_test = train_test_split(X,y, random_state=1)
-vect = CountVectorizer(ngram_range(1,3))
+vect = CountVectorizer(ngram_range=(1,3))
 vect.fit(X_train)
 X_train_dtm = vect.transform(X_train)
 X_test_dtm = vect.transform(X_test)
